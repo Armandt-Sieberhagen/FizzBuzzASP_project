868e96ff2f5efee7c33e60faeb48a6df67423c3f
Armandt-Sieberhagen <84065645+Armandt-Sieberhagen@users.noreply.github.com>
2022-05-30 08:53:32 +0200

    Add files via upload

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Armandt_Sieberhagen_SEESA_APP.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string[] CollectionOfWords = new string[100];
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            
+            for (int i = 1; i < 101; i++)
+            {
+                //creates a small function that takes in a multiple and returns a bool
+                bool isImultipleOf(int multiple) => i % multiple == 0  ? true : false;
+                bool multpleOfFive = isImultipleOf(5);
+                bool multpleOfThree = isImultipleOf(3);
+                CollectionOfWords[i-1] = ConvertNumIntoFizzBuzz(i, multpleOfFive, multpleOfThree);
+            }
+        }
+
+        //logic behind converting the i into a FizzBuzz Value
+        string ConvertNumIntoFizzBuzz(int num,bool multpleOfFive, bool multpleOfThree)
+        {
+            string returnedValue = "";
+            if (multpleOfFive && multpleOfThree) returnedValue = "FizzBuzz";
+            else if (multpleOfThree) returnedValue = "Fizz";
+            else if (multpleOfFive ) returnedValue = "Buzz";
+            else returnedValue = num.ToString();
+            return returnedValue;
+        }
+    }
+}
---
+++ Pages/Project.cs
@@ -0,0 +1,8 @@
+﻿namespace Armandt_Sieberhagen_SEESA_APP.Pages
+{
+    public class Project
+    {
+        public int AmountOfLines { get; set; }
+        public string[] ArrayOfDisplayables { get; set; }
+    }
+}
